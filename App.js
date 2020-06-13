282b2f365c47cd89c3e280ef31d3ed3562dc1ca8
Jeremy Ho <eneka.ken@gmail.com>
2020-06-12 18:23:07 -0700

    refactored login page

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
-import {useState } from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import {useState, useEffect } from 'react';
+import { Platform, StatusBar, StyleSheet, View, Text, TextInput, Button } from 'react-native';
 import * as Font from 'expo-font';
 import {AppLoading} from 'expo';
 
 import useCachedResources from './hooks/useCachedResources';
 import BottomTabNavigator from './navigation/BottomTabNavigator';
 import LinkingConfiguration from './navigation/LinkingConfiguration';
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+      apiKey: "AIzaSyD-LXBYHztZ0-yhAcP7v5dALaoeOrRy_l4",
+      authDomain: "hr-mvp-74f84.firebaseapp.com",
+      databaseURL: "https://hr-mvp-74f84.firebaseio.com",
+      projectId: "hr-mvp-74f84",
+      storageBucket: "hr-mvp-74f84.appspot.com",
+      messagingSenderId: "737251384820",
+      appId: "1:737251384820:web:2db932b1c20fb753806da1",
+      measurementId: "G-XK6R1H55D0"
+    };
+
+   firebase.initializeApp(firebaseConfig);
+
+   function onAuthStateChange(callback) {
+    return 
+  }
+
+
 
 const Stack = createStackNavigator();
 
@@ -23,7 +43,27 @@ const fetchFonts = () => {
 export default function App(props) {
   const isLoadingComplete = useCachedResources();
   const [dataLoaded, setDataLoaded] = useState(false);
+  const [userauth, setuserAuth] = useState(false)
+  const [view, setView] = useState('login')
+  const [email, setEmail] = useState(null)
+  const [password, setPassword] = useState(null)
+  const [confirmpassword, setConfirmPassword] = useState(null)
+  const [loginerror, setLoginmessage] = useState("")
+  const [name, setName] = useState(null)
+  const [signupMessage, setsignupMessage] = useState(null)
 
+ useEffect(() => {
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      setuserAuth({loggedIn: true, email: user.email});
+    } else {
+      setuserAuth({loggedIn: false});
+    }
+  });
+  }, []);
+
+  
+  
   if(!dataLoaded){
     return (
       <AppLoading
@@ -34,7 +74,172 @@ export default function App(props) {
   }
   if (!isLoadingComplete) {
     return null;
-  } else {
+  }
+  if (!userauth.loggedIn){
+    if (view === 'login')
+    return (
+      //<View style={styles.container}>
+      <View style={styles.signup}>
+      <Text>Welcome to the RCJ Fitness App!</Text>
+      <Text> Please login or signup to continue</Text>
+      <Text></Text>
+      <TextInput style={{
+        height: 30,
+        width: 100,
+        borderColor: 'gray',
+        borderWidth: 1
+      }}
+      onChangeText={text => setEmail(text)}
+      placeholder = "Username"/>
+
+      <TextInput style={{
+        height: 30,
+        width: 100,
+        borderColor: 'gray',
+        borderWidth: 1
+      }}
+      onChangeText = {text => setPassword(text)}
+      secureTextEntry = {true}
+      placeholder = "Password"/>
+
+      <View style={styles.fixToText}>
+        <Text style={{ color: 'red' }}>{loginerror}</Text>
+      <Button
+      title = "Login"
+      onPress = {()=> {
+        if (email === null){
+          setLoginmessage("username field cannot be empty")
+        }
+        
+        
+        else {firebase
+        .auth()
+        .signInWithEmailAndPassword(email,password)
+        .catch(error =>{
+          var errorCode = error.code;
+            var errorMessage = error.message;
+              setLoginmessage(error.message)
+            })}
+          }
+        }
+      />
+      <Button
+      title = "Signup"
+      onPress = { () => setView("signup")}
+      />
+      <Button
+      title = "Forgot Password"
+      onPress = {() =>
+        firebase
+        .auth()
+        .sendPasswordResetEmail(
+          email)
+          .then(() => {
+            setLoginmessage('password reset link sent')
+            // Password reset email sent.
+          })
+          .catch(error =>{
+            var errorCode = error.code;
+              var errorMessage = error.message;
+                setLoginmessage( error.message)
+              })
+      }/>
+      </View>
+    </View>
+    )
+    if (view === 'signup')
+      return (
+        <View style={styles.signup}>
+          <TextInput style={{
+              height: 30,
+              width: 100,
+              borderColor: 'gray',
+              borderWidth: 1
+            }}
+            onChangeText={text => setName(text)}
+            placeholder = "Name"/>
+          <TextInput style={{
+              height: 30,
+              width: 100,
+              borderColor: 'gray',
+              borderWidth: 1
+            }}
+            onChangeText={text => setEmail(text)}
+            placeholder = "email"/>
+          <TextInput style={{
+              height: 30,
+              width: 100,
+              borderColor: 'gray',
+              borderWidth: 1
+            }}
+            secureTextEntry = {true}
+            onChangeText={text => setPassword(text)}
+            placeholder = "password"/>
+          <TextInput style={{
+              height: 30,
+              width: 100,
+              borderColor: 'gray',
+              borderWidth: 1
+            }}
+            secureTextEntry = {true}
+            onChangeText={text => setConfirmPassword(text)}
+            placeholder = "confirm password"/>
+            <Text style={{ color: 'red' }}>{signupMessage}</Text>
+            <View style={styles.fixToText}>
+            <Button
+            title = "Submit"
+            onPress = { () => {
+
+              if (password != confirmpassword){
+                setsignupMessage('Passwords do not match')
+            }
+            else if( name === null){
+              setsignupMessage('Name field cannot be blank')
+            }
+            else if( email === null){
+              setsignupMessage('Email field cannot be blank')
+            }
+            else {
+
+                firebase
+              .auth()
+              .createUserWithEmailAndPassword(email, password)
+              
+              .catch(error =>{
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                setsignupMessage(error.message)
+              })
+              .then (()=>{
+                firebase.auth().currentUser.updateProfile({
+                  displayName: name,
+                  photoURL: "https://example.com/jane-q-user/profile.jpg"
+                }).then(function() {
+                
+                }).catch(function(error) {
+                  // An error happened.
+                });
+
+              })
+            }
+          }
+          }
+            />
+
+            <Button
+            title = "Go Back"
+            onPress = {() => setView( "login")}
+            />
+            </View>
+        </View>
+      )
+
+  }
+  
+
+  
+  
+  else {
     return (
       <View style={styles.container}>
         {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
@@ -54,5 +259,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  signup: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+
   },
 });
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,18 +6,7 @@ import * as firebase from 'firebase';
 import axios from 'axios'
 import { ProgressCircle } from 'react-native-svg-charts';
 import { Card, Icon} from 'react-native-elements'
-const firebaseConfig = {
-      apiKey: "AIzaSyD-LXBYHztZ0-yhAcP7v5dALaoeOrRy_l4",
-      authDomain: "hr-mvp-74f84.firebaseapp.com",
-      databaseURL: "https://hr-mvp-74f84.firebaseio.com",
-      projectId: "hr-mvp-74f84",
-      storageBucket: "hr-mvp-74f84.appspot.com",
-      messagingSenderId: "737251384820",
-      appId: "1:737251384820:web:2db932b1c20fb753806da1",
-      measurementId: "G-XK6R1H55D0"
-    };
 
-   firebase.initializeApp(firebaseConfig);
 
 
 const styles = StyleSheet.create({
@@ -172,7 +161,14 @@ export default class HomeScreen extends React.Component {
   this.state = {
     name: null,
     initializing : true,
-    user: null,
+    user: {workouts: 
+      [{date: "6/10/2020",
+      workoutId: 1,
+      calories: Math.ceil(Math.random() * 2000),
+      BPM: Math.ceil(Math.random() * (200 - 60) + 60),
+      duration: Math.ceil(Math.random() * 600),
+      group: "arms"}]}
+    ,
     userauth: null,
     subscriber: "",
     email: null,
@@ -182,10 +178,7 @@ export default class HomeScreen extends React.Component {
     confirmpassword: "",
     loginerror: ""
   }
-  this.handleemailinput = this.handleemailinput.bind(this);
-  this.passwordinput = this.passwordinput.bind(this);
-  this.handlenameinput = this.handlenameinput.bind(this)
-  this.handleconfirmpassword = this.handleconfirmpassword.bind(this)
+ 
   }
   componentDidMount(){
    firebase.auth().onAuthStateChanged(user => {
@@ -218,202 +211,8 @@ export default class HomeScreen extends React.Component {
     );
 
   }
-    handleemailinput(e){
-      this.setState({
-        email: e
-      })
-    }
-    passwordinput(e){
-      this.setState({
-        password: e
-      })
-    }
-    handlenameinput(e){
-      this.setState({
-        name: e
-      })
-    }
-    handleconfirmpassword(e){
-      this.setState({
-        confirmpassword: e
-      })
-    }
-
-  // useEffect(() => {
-  //   const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
-  //   return subscriber;
-  // }, []);
-
-
-
-
+    
 render(){
-
-  //if (this.initializing) return null;
-  if (!this.state.userauth){
-    if (this.state.view === 'login')
-    return (
-      //<View style={styles.container}>
-      <View style={styles.signup}>
-      <Text>Welcome to the RCJ Fitness App!</Text>
-      <Text> Please login or signup to continue</Text>
-      <Text></Text>
-      <TextInput style={{
-        height: 30,
-        width: 100,
-        borderColor: 'gray',
-        borderWidth: 1
-      }}
-      onChangeText={text => this.handleemailinput(text)}
-      placeholder = "Username"/>
-
-      <TextInput style={{
-        height: 30,
-        width: 100,
-        borderColor: 'gray',
-        borderWidth: 1
-      }}
-      onChangeText = {text => this.passwordinput(text)}
-      secureTextEntry = {true}
-      placeholder = "Password"/>
-
-      <View style={styles.fixToText}>
-        <Text style={{ color: 'red' }}>{this.state.loginerror}</Text>
-      <Button
-      title = "Login"
-      onPress = {()=> {
-        if (this.state.email === null){
-          this.setState({loginerror: "username field cannot be empty"})
-        }
-        
-        
-        else {firebase
-        .auth()
-        .signInWithEmailAndPassword(this.state.email, this.state.password)
-        .catch(error =>{
-          var errorCode = error.code;
-            var errorMessage = error.message;
-              this.setState({loginerror : error.message})
-            })}
-          }
-        }
-      />
-      <Button
-      title = "Signup"
-      onPress = { () => this.setState({ view: "signup"})}
-      />
-      <Button
-      title = "Forgot Password"
-      onPress = {() =>
-        firebase
-        .auth()
-        .sendPasswordResetEmail(
-          this.state.email)
-          .then(() => {
-            this.setState({loginerror : 'password reset link sent'})
-            // Password reset email sent.
-          })
-          .catch(error =>{
-            var errorCode = error.code;
-              var errorMessage = error.message;
-                this.setState({loginerror : error.message})
-              })
-      }/>
-      </View>
-    </View>
-      //</View>
-    )
-    if (this.state.view === 'signup')
-return (
-    <View style={styles.signup}>
-      <Text></Text>
-      <TextInput style={{
-          height: 30,
-          width: 100,
-          borderColor: 'gray',
-          borderWidth: 1
-        }}
-        onChangeText={text => this.handlenameinput(text)}
-        placeholder = "Name"/>
-      <TextInput style={{
-          height: 30,
-          width: 100,
-          borderColor: 'gray',
-          borderWidth: 1
-        }}
-        onChangeText={text => this.handleemailinput(text)}
-        placeholder = "email"/>
-      <TextInput style={{
-          height: 30,
-          width: 100,
-          borderColor: 'gray',
-          borderWidth: 1
-        }}
-        secureTextEntry = {true}
-        onChangeText={text => this.passwordinput(text)}
-        placeholder = "password"/>
-      <TextInput style={{
-          height: 30,
-          width: 100,
-          borderColor: 'gray',
-          borderWidth: 1
-        }}
-        secureTextEntry = {true}
-        onChangeText={text => this.handleconfirmpassword(text)}
-        placeholder = "confirm password"/>
-        <Text style={{ color: 'red' }}>{this.state.signupMessage}</Text>
-        <View style={styles.fixToText}>
-        <Button
-        title = "Submit"
-        onPress = { () => {
-
-          if (this.state.password != this.state.confirmpassword){
-            this.setState({signupMessage : 'Passwords do not match'
-          })
-        }
-        else if( this.state.name === null){
-          this.setState({signupMessage: 'Name field cannot be blank'})
-        }
-        else if( this.state.email === null){
-          this.setState({signupMessage: 'Email field cannot be blank'})
-        }
-        else {
-
-           firebase
-          .auth()
-          .createUserWithEmailAndPassword(this.state.email, this.state.password)
-          
-          .catch(error =>{
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            this.setState({signupMessage : error.message})
-          })
-          .then (()=>{
-            firebase.auth().currentUser.updateProfile({
-              displayName: this.state.name,
-              photoURL: "https://example.com/jane-q-user/profile.jpg"
-            }).then(function() {
-              () => this.componentDidMount()
-            }).catch(function(error) {
-              // An error happened.
-            });
-
-          })
-        }
-      }
-     }
-        />
-
-        <Button
-        title = "Go Back"
-        onPress = {() => this.setState({ view: "login"})}
-        />
-        </View>
-    </View>
-)
-
-  }
-
   const workoutcard = this.state.user.workouts.map(((workout, index ) => 
   <Card key={index} title="Workout Name">
   <Text>Workout Name</Text>
@@ -460,6 +259,7 @@ return (
   </View>
 </Card>))
 
+
   return (
     <View style={styles.container} contentContainerStyle={styles.contentContainer}>
       {this.state.user ?
@@ -471,7 +271,7 @@ return (
             source={{
               uri:`${this.state.user.profileImage}`}} />
           <View style={styles.userNameBar}>
-          <Text style={styles.userNameBarText}>Welcome {this.state.userauth.displayName}!</Text>
+          <Text style={styles.userNameBarText}>Welcome {this.state.user.name}!</Text>
         </View>
         </View>
 
@@ -522,7 +322,7 @@ return (
       title = "Logout"
       onPress = {()=> firebase.auth().signOut()
         .then(() => {
-          this.setState({userauth:null, view: "login"})
+          this.setState({loggedIn: false})
         })
         .catch(function(error) {
           // An error happened
@@ -533,7 +333,4 @@ return (
     </View>
   );
   }
-
-
-
   }
